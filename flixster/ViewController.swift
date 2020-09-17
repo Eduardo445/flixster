5cd5513336273685a05cd67465af43796c6d2d4e
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-09-16 17:13:49 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by user180060 on 9/16/20.
+//  Copyright © 2020 lalosoto14@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
