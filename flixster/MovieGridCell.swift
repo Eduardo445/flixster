e7b45a9af58d9165b7e750d3e165c77beba02aa0
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-09-21 17:58:57 -0700

    Finished Assignment 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by user180060 on 9/21/20.
+//  Copyright © 2020 lalosoto14@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
